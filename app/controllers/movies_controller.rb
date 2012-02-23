fa1ec1ceab0adf319c9294801e9bdd340ede31f3
smikHome <soumikb@gmail.com>
2012-02-23 23:20:29 +0530

    Functionality to create new entries

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,4 +9,10 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) #lookup movie by unique ID
     #will render app/views/movies/show.html.haml by default
   end
+  def new
+    #default: render 'new' template
+	@movie = Movie.create!(params[:movie])
+	flash[:notice] = "#{movie.title} was successfully created."
+	redirect_to movies_path
+  end
 end
