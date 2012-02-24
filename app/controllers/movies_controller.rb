c6ba12242560e4de39797aac6616ecf8a41507f7
SoumikVM <soumikb@gmail.com>
2012-02-24 10:16:49 +0000

    Added the remaining CRUD parts from the book

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,10 +9,24 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) #lookup movie by unique ID
     #will render app/views/movies/show.html.haml by default
   end
-  def new
-    #default: render 'new' template
-	@movie = Movie.create!(params[:movie])
-	flash[:notice] = "#{movie.title} was successfully created."
-	redirect_to movies_path
+  def create 
+	  @movie = Movie.create!(params[:movie])
+	  flash[:notice] = "#{@movie.title} was successfully created."
+	  redirect_to movies_path
+  end
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  def update
+    @movie =  Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
   end
 end
